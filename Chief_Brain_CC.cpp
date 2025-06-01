3e5ddac099cc0f781616e877ce57b39408b966b8
ViPlav Kumar <155620870+iamviplavkr@users.noreply.github.com>
2025-06-01 09:36:05 +0530

    Create Chief_Brain_CC.cpp

---
+++ Chief_Brain_CC.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    if(x>y)
+    {
+        cout << "NO";
+    }
+    else if(y>x)
+    {
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    
+}
