d1641b9b963014b0075ec74f983c3afdb467b231
ViPlav Kumar <155620870+iamviplavkr@users.noreply.github.com>
2025-06-01 09:37:54 +0530

    Update Chief_Brain_CC.cpp

--- Chief_Brain_CC.cpp
+++ Chief_Brain_CC.cpp
@@ -1,3 +1,10 @@
+/*Chef and Brain Speed
+
+In ChefLand, human brain speed is measured in bits per second (bps). Chef has a threshold limit of XX bits per second above which his calculations are prone to errors. If Chef is currently working at YY bits per second, is he prone to errors?
+
+If Chef is prone to errors print YES, otherwise print NO.*/
+
+
 #include <iostream>
 using namespace std;
 
